33e15869b9665882a8ae0bf5a490b956fafdf6f4
Aatir Nadim <aatir.nadim@gmail.com>
2023-01-21 21:10:06 +0530

    setting up firebase database

--- src/Components/Events.js
+++ src/Components/Events.js
@@ -1,11 +1,25 @@
 import React from "react";
 
+import { db } from "../firebaseConfig";
 
+// import { getDatabase, ref, onValue} from "firebase/database";
+import { getDatabase, ref, child, get } from "firebase/database";
 
 export const Events = () => {
+
+    const dbRef = ref(getDatabase());
+    get(child(dbRef, ``)).then((snapshot) => {
+        if (snapshot.exists()) {
+            console.log(snapshot.val());
+        } else {
+            console.log("No data available");
+        }
+    }).catch((error) => {
+        console.error(error);
+    });
     return (
         <div>
-        <h1>Events</h1>
+            <h1>Events</h1>
         </div>
     );
 }
