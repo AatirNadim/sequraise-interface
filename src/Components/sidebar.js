73415de6f510dd97dbc56851bd4604ece1d3e47b
Aatir Nadim <aatir.nadim@gmail.com>
2023-01-22 13:33:52 +0530

    mock up updates

--- src/Components/sidebar.js
+++ src/Components/sidebar.js
@@ -2,16 +2,46 @@ import React from 'react'
 
 const Sidebar = () => {
     return (
-        <div>
-            I am a sidebar
+        <div
+        style={{backgroundColor : '#00b8f1',
+        height : '95vh',
+        display : 'flex',
+        flexdirection : 'column', 
+        justifyContent : 'space-between',
+        color : 'white',
+        flexFlow : 'column',
+
+    }}
+        
+        >
+            {/* {'I am a sidebar'} */}
             {/* we have a icon here  */}
-            <div>
-                <img src="" alt="" />
+            <div
+            style={{
+                // height : 'fit-content',
+            }}
+            >
+                <img src="/Hamburger_icon.svg.png" alt="sandwich icon" 
+                style={{
+                    height : '50px',
+                    marginLeft : '7px',
+                    color : 'white',
+                }}
+                />
             </div>
             {/* we have another  icon here */}
-            <div>
-                <img src="" alt="" />
+            
+            <div>   
+                <img src="/arrow_inside_box.png" alt="arrow icon" 
+                
+                style={{
+                    height : '60px',
+                    marginLeft : '0px',
+                    color : 'white',
+                }}
+                />
             </div>
+            
         </div>
     )
 }
--- src/Components/topbar.js
+++ src/Components/topbar.js
@@ -1,18 +1,36 @@
 import React from 'react'
 
 const Topbar = () => {
+    // const useContext = React.useContext(Usercontext);
     return (
         <div
             style={{
                 height: "5vh",
-                backgroundColor: "red",
+                backgroundColor: "#001c7b",
                 display: "flex",
                 justifyContent: "space-between",
                 alignItems: "center",
                 padding: "0 20px"
 
             }}
-        >Topbar</div>
+        >
+            <div 
+            style={{
+                color: "#018581",
+                fontFamily : 'serif',
+                fontWeight: "bold",
+            }}
+            >
+                SEQUR
+                <span
+                style={{
+                    color: "#e6020c",
+                    fontSize : '20px'
+                }}
+                >AI</span>
+                SE
+            </div>
+        </div>
     )
 }
 
